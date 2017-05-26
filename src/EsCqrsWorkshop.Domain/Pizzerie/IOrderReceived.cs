5a7ed0ff856e4f6f4b672f8ec64016f1950fd161
Nazareno Manco <nazareno.m@live.com>
2017-05-27 01:04:46 +0200

    Add Order Command & Event

---
+++ src/EsCqrsWorkshop.Domain/Pizzerie/IOrderReceived.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Radical.CQRS;
+
+namespace EsCqrsWorkshop.Domain.Pizzerie
+{
+    public interface IOrderReceived : IDomainEvent
+    {
+        Guid OrderId { get; set; }
+        string CustomerName { get; set; }
+        string PizzaTaste { get; set; }
+    }
+}
